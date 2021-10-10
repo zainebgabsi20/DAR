ceace5f7e9f80df4d96dc99bc1764bdfd9ba4e5c
MHZ <mohamed.hichem.zayani@gmail.com>
2021-10-10 21:36:59 +0100

    comment main method

--- src/tp1/InheritanceChallenge.java
+++ src/tp1/InheritanceChallenge.java
@@ -7,19 +7,19 @@ public class InheritanceChallenge {
     static class C extends B {}
 
     public static void main(String[] args) {
-        A object1 = new A();
-        B object2 = new B();
-        C object3 = new C();
-        B object4 = new C();
-        A object5 = new C();
-
-        //let the challenge begin : which ones are right ?
-        object1=object2;
-        object1=object3;
-        object3=object1;
-        object4=object5;
-        object3=(C)object1;
-        object4=(B)object5;
-        object3=(B)object5;
+//        A object1 = new A();
+//        B object2 = new B();
+//        C object3 = new C();
+//        B object4 = new C();
+//        A object5 = new C();
+//
+//        //let the challenge begin : which ones are right ?
+//        object1=object2;
+//        object1=object3;
+//        object3=object1;
+//        object4=object5;
+//        object3=(C)object1;
+//        object4=(B)object5;
+//        object3=(B)object5;
     }
 }
