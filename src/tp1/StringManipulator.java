03df7d9655744ae92d908108268935df5a04fb63
zainebgabsi20 <84251771+zainebgabsi20@users.noreply.github.com>
2021-10-30 00:06:21 +0200

    Update StringManipulator.java

--- src/tp1/StringManipulator.java
+++ src/tp1/StringManipulator.java
@@ -8,33 +8,50 @@ public class StringManipulator {
         this.s = s;
     }
 
-    public void reverse(){
-        //do your stuff
-        System.out.println("reverse : ");
+    public void reverse() {
+        String reversed = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            reversed = reversed + s.charAt(i);
+        }
+
+        System.out.println("reverse : "+reversed);
     }
 
     public void isPalindrome(){
-        //do your stuff
-        System.out.println("isPalindrome : ");
+        int i=0;
+        boolean palindrome=true ;
+        while(i<(s.length()-1)/2 && palindrome){
+            if(s.charAt(i)==s.charAt((s.length()-1)-i))
+                palindrome = true;
+            else
+                palindrome = false;
+            i++;
+        }
+        System.out.println("isPalindrome : "+palindrome);
     }
 
     public void toUpperCase(){
-        //do your stuff
-        System.out.println("uppercase : ");
+        System.out.println("uppercase : "+s.toUpperCase());
     }
 
     public void toLowerCase(){
-        //do your stuff
-        System.out.println("lowercase : ");
+        System.out.println("lowercase : "+s.toLowerCase());
     }
 
     public void getVowelNumber(){
-        //do your stuff
-        System.out.println("vowelNumber : ");
+        int vowel = 0;
+        String c = s.toLowerCase();
+        for(int i = 0; i < c.length(); i++) {
+            char v = c.charAt(i);
+            if(v == 'a' || v == 'e' || v == 'i' || v == 'o' || v == 'u') {
+                vowel++;
+            }
+        }
+        System.out.println("vowelNumber : "+vowel);
     }
 
     public static void main(String[] args) {
-        StringManipulator stringManipulator = new StringManipulator("tene");
+        StringManipulator stringManipulator = new StringManipulator("teNe");
         stringManipulator.reverse();
         stringManipulator.isPalindrome();
         stringManipulator.toUpperCase();
